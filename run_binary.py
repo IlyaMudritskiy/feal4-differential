dcc2a9e00cb04b07e48ecfbe0e7235f789a12919
IlyaMudritskiy <ilya.mudritskiy@gmail.com>
2022-11-28 13:57:26 +0000

    Completed code for automated key checking for all plaintext-ciphertext pairs

---
+++ run_binary.py
@@ -0,0 +1,67 @@
+import os
+from dataclasses import dataclass
+
+@dataclass
+class CheckKeys:
+    bin_name: str = ""
+
+    # Plaintext - ciphertext pairs
+    plaintext: list = []
+    ciphertext: list = []
+
+    def transform_text(self, text: int) -> str:
+        """Transform int text to d4 43 3c e1 79 61 48 2f."""
+
+        start = 0
+        stop = 2
+        res = ""
+
+        for _ in range(8):
+            b = str(hex(text)[2:])[start:stop]
+            res += b + " "
+            start += 2
+            stop += 2
+        
+        return res
+    
+    def plaintext_to_input(self, plaintext: int, ciphertext: int) -> str:
+        """
+        Outputs the plaintext and corresponding ciphertext 
+        to be passed as an argument to program as:
+
+        d4 43 3c e1 79 61 48 2f 7a 9a a3 55 4e b7 ff ee
+        |------Plaintext------| |------Ciphertext-----|
+        """
+
+        return f"{self.transform_text(plaintext)}{self.transform_text(ciphertext)}"
+
+
+    def run_file(self, plaintext: int, ciphertext: int) -> None:
+        """
+        Run the compiled code for checking keys.
+        The output of binary will be put into a file.
+        """
+
+        params = self.plaintext_to_input(plaintext, ciphertext)
+        command = f"./{self.bin_name} {params}"
+        stream = os.popen(command)
+        output = stream.read()
+
+        try:
+            with open("output.txt", "a") as f:
+                f.write(output)
+        except Exception as e:
+            print(e)
+
+        stream.close()
+
+    def check(self):
+        for i, value in self.plaintext:
+            print(f"[STARTED]    Current step: {i}/{len(value)}")
+            self.run_file(value)
+            i += 1
+
+
+if __name__ == "__main__":
+    check_keys = CheckKeys()
+    check_keys.check()
